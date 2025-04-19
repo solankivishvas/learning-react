0976d59a9a2df938fd4e23eb8e2e9c7043530e61
VishvasSolanki <vishvassolanki08@gmail.com>
2025-04-19 11:14:25 +0530

    Counter DOne

--- counter-project/src/App.jsx
+++ counter-project/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    let [counter, setCounter] = useState(0);
+
+    // let counter = 10;
+
+    const addValue = () => {
+        console.log("Value added", counter);
+        if (counter == 100) {
+            console.log("You can't go up above 100");
+        } else {
+            counter = counter + 1;
+            setCounter(counter);
+        }
+    };
+
+    const removeValue = () => {
+        console.log("Value removed", counter);
+        if (counter == 0) {
+            console.log("Value cannot go down below 0");
+        } else {
+            counter = counter - 1;
+            setCounter(counter);
+        }
+    };
+
+    return (
+        <>
+            <h1>Counter Project</h1>
+            <h2>Counter value: {counter}</h2>
+            <button onClick={addValue}>Increase Value</button>
+            <button onClick={removeValue}>Decrease Value</button>
+            <p>The counter can work only within 0 to 100.</p>
+        </>
+    );
 }
 
-export default App
+export default App;
