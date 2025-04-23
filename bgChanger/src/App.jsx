7f46976ad7acf03a9750a1bf86a9bcb02fca01dd
VishvasSolanki <vishvassolanki08@gmail.com>
2025-04-23 17:38:47 +0530

    Bug fixed and feat added

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-    let [color, setColor] = useState("bg-purple-400");
+    let [color, setColor] = useState("bg-black");
 
     function bgChange(newColor) {
         setColor(newColor);
@@ -9,7 +9,7 @@ function App() {
 
     return (
         <>
-            <div className={`w-full bg-purple-400 h-screen ${color} `}>
+            <div className={`w-full h-screen ${color} `}>
                 <div className="flex gap-4 justify-center pt-5 ">
                     <button
                         onClick={() => bgChange("bg-red-600")}
@@ -28,6 +28,24 @@ function App() {
                         className="border-2 pl-5 pr-5 pt-3 pb-3 rounded-sm bg-blue-600"
                     >
                         Blue
+                    </button>
+                    <button
+                        onClick={() => bgChange("bg-orange-600")}
+                        className="border-2 pl-5 pr-5 pt-3 pb-3 rounded-sm bg-orange-600"
+                    >
+                        Orange
+                    </button>
+                    <button
+                        onClick={() => bgChange("bg-pink-600")}
+                        className="border-2 pl-5 pr-5 pt-3 pb-3 rounded-sm bg-pink-600"
+                    >
+                        Pink
+                    </button>
+                    <button
+                        onClick={() => bgChange("bg-purple-600")}
+                        className="border-2 pl-5 pr-5 pt-3 pb-3 rounded-sm bg-purple-600"
+                    >
+                        Purple
                     </button>
                 </div>
             </div>
